e789bd370b045fdc16077f34234092126a5b70fa
wduf <wduf02@gmail.com>
2023-01-15 12:05:31 -0500

    Updates

---
+++ _app/immutable/chunks/2-770b79f5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-14dddd24.js";export{t as component};
--- _app/immutable/chunks/2-dff1b95b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-f98b96d1.js";export{t as component};
