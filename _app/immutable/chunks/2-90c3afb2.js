1c4b1e38fc25c3d32d144d81e4578d8fecd6e9fc
wduf <wduf02@gmail.com>
2023-01-15 02:32:32 -0500

    Updates

---
+++ _app/immutable/chunks/2-90c3afb2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-cbc7bf3a.js";export{t as component};
--- _app/immutable/chunks/2-fe62d9e0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-33b71193.js";export{t as component};
